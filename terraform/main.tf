3f8b489c810a226a7c6e655796a36897cc0b32bd
Salodoxera <DavidDubovik19@gmail.com>
2025-09-18 18:20:26 +0300

    update ci/cd

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,5 +10,5 @@ provider "aws" {
   # В access_key і secret_key вказуємо нашого user якого ми створили в aws 
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region     = var.region # Це регіон AWS, де створюватимуться ресурси
+  region     = "eu-north-1" # Це регіон AWS, де створюватимуться ресурси
 }
